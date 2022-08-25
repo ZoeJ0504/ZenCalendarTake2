d6e1a4fdf64a4e79b21ed982f962bea13f338b0b
Connor Henn <connor.henn@yahoo.com>
2022-08-25 16:29:34 -0700

    grocery list

--- src/Page/Food.js
+++ src/Page/Food.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import RecipeCard from '../components/RecipeCard'
+import styled from 'styled-components';
+import GroceryList from '../components/GroceryList';
 
 function Food() {
+
     const [recipes, setRecipes] = useState({})
     const [foodGroup, setFoodGroup] = useState('')
     const [input, setInput] = useState('');
+    const [groceryList, setGroceryList] = useState([]);
 
     useEffect(() => {
-        fetch(`https://recipesapi2.p.rapidapi.com/recipes/${foodGroup}`, {
+        fetch(`https://recipesapi2.p.rapidapi.com/recipes/pancakes`, {
             method: "GET",
             params: { "maxRecipes": '10' },
             headers: {
@@ -33,19 +37,58 @@ function Food() {
         
     }
 
+    function addIngredients(recipeName){
+
+        fetch(`https://recipesapi2.p.rapidapi.com/recipes/${recipeName}`, {
+            method: "GET",
+            params: { "maxRecipes": '1' },
+            headers: {
+                'X-RapidAPI-Key': 'f155deaea6msh7af5e02097744dfp103350jsnff9f118520c5',
+                'X-RapidAPI-Host': 'recipesapi2.p.rapidapi.com'
+            }
+        })
+        .then (res => res.json())
+        .then(result => {
+            const newIngredients = result.data.find(recipe => recipe.name === recipeName).ingredients
+            setGroceryList(list => [...list, ...newIngredients])
+        })
+        
+    }
+
+
     return (
-        <div>
-            <p>Look up Recipes for your Favorite Meals</p>
+        <FoodPageDiv>
+            <FoodCardDiv>
+                <p>Look up Recipes for your Favorite Meals</p>
 
-            <form onSubmit={handleSubmit}> 
-                <input onChange={handleOnChange} type="text" value={foodGroup} placeholder="FoodGroup"></input>
-                <button type='Submit'>Search</button>
-            </form>
+                <form onSubmit={handleSubmit}> 
+                    <input onChange={handleOnChange} type="text" value={foodGroup} placeholder="FoodGroup"></input>
+                    <button type='Submit'>Search</button>
+                </form>
 
 
-            {recipeArr === undefined ? <p>Loading...</p> : recipeArr.map((recipe) => <RecipeCard key={recipeArr.indexOf(recipe)} recipe={recipe} />)}
-        </div>
+                {recipeArr === undefined ? <p>Loading...</p> : recipeArr.map((recipe) => <RecipeCard key={recipeArr.indexOf(recipe)} recipe={recipe} addIngredients={addIngredients}/>)}
+            </FoodCardDiv>
+
+            <GroceryListDiv>
+                <h1>Grocery List</h1>
+                <GroceryList groceryList={groceryList}/>
+            </GroceryListDiv>
+        </FoodPageDiv>
     )
 }
 
-export default Food
+const FoodPageDiv = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 100px`
+
+
+const FoodCardDiv = styled.div`
+
+`
+
+const GroceryListDiv = styled.div`
+`
+
+export default Food;
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
-function RecipeCard({recipe}) {
+function RecipeCard({recipe, addIngredients}) {
 
     const [commentsVisible, setCommentsVisible] = useState(false);
 
     function handleClick(){
-        setCommentsVisible(commentsVisible => !commentsVisible);
-        
+        setCommentsVisible(commentsVisible => !commentsVisible);    
     }
+
+    const groceryClick = (event)=> {
+        addIngredients(recipe.name);
+    }
+ 
 
     const ingredientText = commentsVisible ? `Ingredients: ${recipe.ingredients}` : 'Click Image for additional cooking instructions';
     const instructionText = commentsVisible ? `Instructions: ${recipe.instructions}` : '';
@@ -24,11 +28,13 @@ function RecipeCard({recipe}) {
             <img onClick={handleClick} src={recipe.image} alt='food' width='250px' height='250px'/>
             <p>{ingredientText}</p>
             <p>{instructionText}</p>
+            <button className={recipe.name} onClick={groceryClick}>Add ingredients to Grocery List</button>
 
         </div>
     )
 
 }
+
 
 
 export default RecipeCard;
